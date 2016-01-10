acede2512e44591ab77131f40678375c7608898c
Kouhei Sutou <kou@clear-code.com>
2016-01-10 14:53:44 +0900

    Update test script for reproducing problem

--- test.sh
+++ test.sh
@@ -6,25 +6,22 @@ ${=MYSQL} < init.sql
 
 while true
 do
-    count=$(${=MYSQL} db_test < count.sql | tail -1)
-
     ${=MYSQL} db_test < recover.sql
     rm -f run.sql
-    for i in {0..$((count / 2))}
+    count=$(${=MYSQL} db_test < count.sql | tail -1)
+    for i in {0..$((count))}
     do
 	${=MYSQL} -N db_test < while.sql >> run.sql
     done
     echo "Null" >> run.sql
-    echo "Null" >> run.sql
 
     ${=MYSQL} db_test < recover.sql
-    (while true
+    ${=MYSQL} db_test < run.sql &
+    run_pid=$!
+    (while [ 0 -ne `ps h ${run_pid}|wc -l` ]
      do
 	 ${=MYSQL} db_test < rollback.sql
-     done) &
-    rollback_pid=$!
-    ${=MYSQL} db_test < run.sql
-    kill ${rollback_pid}
+     done)
 
     if ! diff -u \
 	 <(${=MYSQL} db_test < select-no-index.sql) \
